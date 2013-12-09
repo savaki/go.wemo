f65d3ddd1f2f62e0cf0274a2eddf685e79eb95c8
Matt <matt.ho@gmail.com>
2013-12-08 22:52:37 -0800

    - renmaing starting interface

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 wemo
 ====
+
+```
+package main
+
+import (
+	"fmt"
+	"github.com/savaki/wemo"
+	"time"
+)
+
+func main() {
+  api, _ := wemo.NewByInterface("en0")
+  devices, _ := api.DiscoverAll(3*time.Second)
+  for _, device := range devices {
+    fmt.Printf("Found %+v\n", device)
+  }
+}
+```
+
+```
+package main
+
+import (
+	"fmt"
+	"github.com/savaki/wemo"
+)
+
+func main() {
+  device     := &Device{Host:"10.0.1.32:49153"}
+  deviceInfo := device.FetchDeviceInfo()
+  fmt.Printf("Found => %+v\n", deviceInfo)
+
+  device.On()
+  device.Off()
+}
+```
--- finder.go
+++ finder.go
@@ -12,11 +12,11 @@ type Wemo struct {
 	Debug  bool
 }
 
-func (self *Wemo) FindAll(timeout time.Duration) ([]*Device, error) {
+func (self *Wemo) DiscoverAll(timeout time.Duration) ([]*Device, error) {
 	var all []*Device
 	urns := []string{"urn:Belkin:device:controllee:1", "urn:Belkin:device:light:1", "urn:Belkin:device:sensor:1"}
 	for _, urn := range urns {
-		devices, _ := self.Find(urn, timeout)
+		devices, _ := self.Discover(urn, timeout)
 		for _, device := range devices {
 			all = append(all, device)
 		}
@@ -25,7 +25,7 @@ func (self *Wemo) FindAll(timeout time.Duration) ([]*Device, error) {
 	return all, nil
 }
 
-func (self *Wemo) Find(urn string, timeout time.Duration) ([]*Device, error) {
+func (self *Wemo) Discover(urn string, timeout time.Duration) ([]*Device, error) {
 	locations, err := self.scan(urn, timeout)
 	if err != nil {
 		return nil, err
