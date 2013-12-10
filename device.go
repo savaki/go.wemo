0faa0a404677b7251346038a2021c4943a67706b
Matt <matt.ho@gmail.com>
2013-12-09 23:57:24 -0800

    - never had this much trouble with a print statement

--- device.go
+++ device.go
@@ -103,7 +103,7 @@ func (self *Device) changeState(newState bool) error {
 	message := newSetBinaryStateMessage(newState)
 	response, err := post(self.Host, "SetBinaryState", message)
 	if err != nil {
-		log.Println("unable to SetBinaryState for %+v\n", self)
+		log.Println("unable to SetBinaryState")
 		return err
 	}
 	defer response.Body.Close()
